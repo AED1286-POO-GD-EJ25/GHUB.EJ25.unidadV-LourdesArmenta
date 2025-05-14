324eef186e69ca1f10cc0f1299de311b8e5dac39
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-05-14 15:56:06 +0000

    TestCalcularMediaException

---
+++ miPrincipal/MediaException.java
@@ -0,0 +1,14 @@
+package miPrincipal;
+
+
+
+public class MediaException extends Exception{
+    public MediaException(){
+        super();
+
+    }
+    public MediaException(String mensaje){
+        super(mensaje);
+    }
+    
+}
--- miPrincipal/TestCalcularMediaException.java
+++ miPrincipal/TestCalcularMediaException.java
@@ -1,22 +1,26 @@
 package miPrincipal;
 
 public class TestCalcularMediaException {
-     public static void main(String[] args) {
+     public static void main(String[] args) throws MediaException{
         System.out.println("La media es:"+calcularMedia(8));
-        
     }
-    public static double calcularMedia(int num)  {
+    public static double calcularMedia(int num) throws MediaException  {
         double c[]={1.2,2.2,3.3,4.4,5.5,6.6,7.7};
         double media;
-       
+        try{
             media = avg(c,num);
             return media;
-    
+        }catch(MediaException e){
+            System.out.println("excepcion capturada: "+e);
+            System.out.println("calcula media: uso de la longitud del arreglo");
+            num = c.length;
+            return avg(c,num);        
+        }
         
-        
-
     }
-    public static double avg(double []b,int num) {
+    public static double avg(double []b,int num) throws MediaException{
+        if(num>b.length)
+            throw new MediaException("n es mas grande que el tamaño del arreglo");
         
        
         double media =0;
